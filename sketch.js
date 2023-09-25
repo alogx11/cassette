3ffd0d3765f5f9e3e8dc6a3d845879c6360405c1
alogx11 <alogx11@gmail.com>
2023-09-25 12:13:45 -0500

    added start stop to song and rate change

--- sketch.js
+++ sketch.js
@@ -11,6 +11,43 @@ Modifier Boutton()
 cassettee object ?
 */
 
+let song;
+let duration;
+
 function preload() {
-  song = loadSound("./assets/scv=pit.mp3");
+  song = loadSound("assets/scv-pit.mp3");
+}
+
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+  duration = song.duration;
+}
+
+function draw() {
+  print(song.currentTime());
+  fill(255, 0, 0);
+  let val = map(time, 0, duration, 0, width);
+  rect(0, 0, val, height);
+}
+
+function mousePressed() {
+  print("moues press");
+  if (song.isPlaying()) {
+    song.stop();
+  } else {
+    song.setVolume(0.5);
+    song.play();
+    duration = song.duration;
+    print(duration);
+  }
+}
+
+function keyPressed() {
+  if (key == "r") {
+    song.rate(0.5);
+  }
+  if (key == "p") {
+    song.pan(-1);
+  }
 }
