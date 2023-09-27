0b6c0b32d8f4e77b238d28a90b6c8802ebefd303
alogx11 <alogx11@gmail.com>
2023-09-27 11:45:49 -0500

    added cassette to canvas

--- sketch.js
+++ sketch.js
@@ -13,15 +13,39 @@ cassettee object ?
 
 let song;
 let duration;
+let cassetteImg, reelImg;
+let font;
 
 function preload() {
   song = loadSound("assets/From-the-Start.mp3");
+  cassetteImg = loadImage("assets/cassette.png");
+  font = loadFont("assets/MIXTMK_T.ttf");
+  reelImg = loadImage("assets/wheelorange.png");
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
   background(0);
   duration = song.duration;
 }
 
-function draw() {}
+function draw() {
+  background(255);
+  let position = mouseX + " " + mouseY;
+  fill(0);
+  textSize(20);
+  textFont("Helvetica");
+  text(position, 50, 50);
+  let cassetteScale = 0.5;
+  image(
+    cassetteImg,
+    100,
+    100,
+    cassetteImg.width * cassetteScale,
+    cassetteImg.height * cassetteScale
+  );
+  textSize(15);
+  fill(120);
+  textFont(font);
+  text("sample text", 142, 140);
+}
