4e8af246495c3e253dfd3ad456f6f209d3c516ec
alogx11 <alogx11@gmail.com>
2023-11-05 18:11:48 -0600

    added comments to document reel object

--- reel.js
+++ reel.js
@@ -16,10 +16,15 @@ class Reel {
     this.weight = scale * hue * 0.05;
   }
 
+  /* displays the magtapes and the images of the reels
+  param: increment - added to the angle of the reel to change speed of rotation.
+
+  */
   display(increment) {
     this.displayMagTape();
     this.displayReel();
     if (increment != null) {
+      // wheels spin in different directions when cassette is flipped.
       if (sideButton.getState()) {
         increment *= -1;
       }
@@ -29,17 +34,24 @@ class Reel {
     }
   }
 
+  /*
+  displays the black circles for the mag tapes.
+  */
   displayMagTape() {
-    // tape circle
-    fill(0);
+    // width of tape is dependend on how much of the song has passed
     let tempW = song.currentTime() / song.duration();
+    // size of tape depends on which sise of cassette it is on.
+    // this creates the illusion of the tape moving from one circle to the other
     if (this.left) {
       tempW = 1 - tempW;
     }
+    // map the percentage of the song to the min and max width
     tempW = map(tempW, 0, 1, this.minW, this.maxW);
+    // add a wobble to the tape when wobble effect is enabled
     if (wobble) {
       let wobbleVal;
       if (this.left) {
+        // we want tapes to wobble independently, so move to a different part of the curve if it is the right tape
         wobbleVal = noise(wobbleNoise);
       } else {
         wobbleVal = noise(wobbleNoise + 100);
@@ -50,23 +62,19 @@ class Reel {
     }
     push();
     noStroke();
+    // set stroke is enabled when the song is playing and myVisual is toggled on
     if (this.setStroke) {
       colorMode(HSB, 255);
       stroke(this.hue, 255, 255);
       strokeWeight(this.weight);
     }
+    // draw black circle for tape
+    fill(0);
     ellipse(this.x, this.y, tempW, tempW);
     // white circle in the middle of tape
     fill(255);
-    let val = 0;
-    if (wobble) {
-      let wobbleVal = noise(wobbleNoise + random(0, 10));
-      val = map(wobbleVal, 0, 1, -0.3, 0.3);
-    } else {
-      val = 0;
-    }
     ellipse(
-      this.x + val,
+      this.x,
       this.y,
       this.reelImg.width * this.scale,
       this.reelImg.width * this.scale
@@ -74,10 +82,14 @@ class Reel {
     pop();
   }
 
+  // display image of the reel
   displayReel() {
     push();
+    // set origin to x,y where the reel should be
     translate(this.x, this.y);
+    // rotate the image around the origin
     rotate(radians(this.angle));
+    // origin is set where reel should be, so it is at 0,0
     image(
       this.reelImg,
       0,
@@ -88,10 +100,12 @@ class Reel {
     pop();
   }
 
+  // when the reel is flipped, left becomes right, right becomes left
   flipReel() {
     this.left = !this.left;
   }
 
+  // method called when myButton is toggled on
   giveStroke() {
     if (song.isPlaying() && myButton.toggled) {
       // we are only visualizing when song is playing
@@ -102,6 +116,7 @@ class Reel {
       this.hue = fft.getEnergy("bass");
       this.weight = scale * this.hue * 0.05;
     } else {
+      // no effect when song is not playing or button not toggled
       this.hue = 0;
       this.weight = 0;
       this.setStroke = false;
